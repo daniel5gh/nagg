e27a694070f5fe2bca0c31f6178136b4ad98cad3
Daniël <daniel5git@spiet.nl>
2015-10-05 02:30:57 +0200

    Django Admin for NewsItem

--- nagg/admin.py
+++ nagg/admin.py
@@ -1,4 +1,9 @@
 # coding=utf-8
 from django.contrib import admin
+from .models import NewsItem
 
-# Register your models here.
+
+class NewsItemAdmin(admin.ModelAdmin):
+    list_display = ('publish_date', 'get_title', 'source')
+
+admin.site.register(NewsItem, NewsItemAdmin)
--- nagg/models.py
+++ nagg/models.py
@@ -8,3 +8,11 @@ class NewsItem(models.Model):
     text = models.TextField()
     publish_date = models.DateTimeField()
     retrieval_date = models.DateTimeField()
+
+    def get_title(self):
+        return self.text.split('\n')[0]
+
+    def __str__(self):
+        return self.get_title()
+
+
