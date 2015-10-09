54a3967eaade029bdd3d91c642d398ac0f2a4446
Daniël <daniel5git@spiet.nl>
2015-10-10 00:00:13 +0200

    Date hierarchy

--- nagg/admin.py
+++ nagg/admin.py
@@ -3,9 +3,10 @@ from django.contrib import admin
 from .models import NewsItem
 
 
+@admin.register(NewsItem)
 class NewsItemAdmin(admin.ModelAdmin):
     list_display = ('publish_date', 'get_title', 'source')
     list_display_links = ('get_title',)
     list_filter = ('source',)
+    date_hierarchy = 'publish_date'
 
-admin.site.register(NewsItem, NewsItemAdmin)
