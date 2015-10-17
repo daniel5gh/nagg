3845e97945419f81b98e4fd5df30d7603ac02341
Daniël <daniel5git@spiet.nl>
2015-10-17 21:48:41 +0200

    Leech SRTs

--- nagg/tasks.py
+++ nagg/tasks.py
@@ -19,7 +19,11 @@ def download_youtube(self, url):
     ydl_opts = {
         'logger': _log,
         # 'download_archive': os.path.join(DOWNLOAD_PATH, 'yt_download_archive'),
-        'outtmpl': os.path.join(DOWNLOAD_PATH, '%(uploader)s', DEFAULT_OUTTMPL)
+        'outtmpl': os.path.join(DOWNLOAD_PATH, '%(uploader)s', DEFAULT_OUTTMPL),
+        'writesubtitles': True,
+        'writeautomaticsub': True,
+        'subtitleslangs': ['en'],
+        # 'skip_download': True,
     }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
