6f389e63b6ec33260c83b02fbc27b62414f103f0
Daniël <daniel5git@spiet.nl>
2015-12-26 23:04:37 +0100

    add bootstrap

--- static_root/common.js
+++ static_root/common.js
@@ -6,6 +6,12 @@ requirejs.config({
         d3: 'node_modules/d3/d3.min',
         backbone: 'node_modules/backbone/backbone-min',
         // _ is installed as dependency of bb, in its dir
-        underscore: 'node_modules/backbone/node_modules/underscore/underscore-min'
+        underscore: 'node_modules/backbone/node_modules/underscore/underscore-min',
+        bootstrap: 'node_modules/bootstrap/dist/js/bootstrap',
     }
+});
+
+// load bootstrap jQuery plugins
+require(['jquery'], function ($) {
+    require(['bootstrap']);
 });
