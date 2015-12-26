d0d643cc4dd1fc18ba0a0eac123d67c2aebd924b
Daniël <daniel5git@spiet.nl>
2015-12-27 00:56:20 +0100

    localized dates

--- mainpage/static/mainpage/models/newsitem.js
+++ mainpage/static/mainpage/models/newsitem.js
@@ -2,7 +2,7 @@ define([
     'jquery',
     'underscore',
     'backbone',
-], function($, _, Backbone) {
+], function ($, _, Backbone) {
     return Backbone.Model.extend({
         urlRoot: 'api/v1/newsitems/',
         defaults: {
@@ -12,6 +12,11 @@ define([
             publish_date: '',
             retrieval_date: '',
             _expanded: false,
+        },
+        parse: function (response) {
+            response.publish_date = new Date(response.publish_date).toLocaleString();
+            response.retrieval_date = new Date(response.retrieval_date).toLocaleString();
+            return response;
         }
     });
 });
