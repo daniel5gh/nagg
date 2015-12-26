15ccdd98ae6f787b9cd859cfacd0fe76fcfa520e
Daniël <daniel5git@spiet.nl>
2015-12-26 17:41:46 +0100

    Prevent 10 full updates per sync

--- mainpage/static/mainpage/views/newsitemviews.js
+++ mainpage/static/mainpage/views/newsitemviews.js
@@ -21,21 +21,39 @@ define([
             this.$aPrev = this.$('.link-prev');
             this.$aNext = this.$('.link-next');
 
-            this.collection.on('change reset add remove', function () {
+            // reset the whole view on any sync
+            this.collection.on('sync', function () {
+                self.renderPrevNext();
                 self.$tbody.empty();
                 self.render(self);
             });
+
+            // issue with these events is that we need to keep track of views
+            //this.collection.on('change add remove', function (model) {
+            //    var a = arguments;
+            //    console.log('rest', a);
+            //    //self.$tbody.empty();
+            //    self.renderOne(model);
+            //});
         },
         //events: {
         //    "click #add-friend": "showPrompt",
         //},
+        renderOne: function(model) {
+            var newsItemView = new NewsItemRow({model: model});
+            this.$tbody.append(newsItemView.render().$el);
+
+            return newsItemView;
+        },
         render: function () {
             var self = this;
             this.collection.each(function (newsItem) {
-                var newsItemView = new NewsItemRow({model: newsItem});
-                self.$tbody.append(newsItemView.render().$el);
+                self.renderOne(newsItem);
             });
 
+            return this;
+        },
+        renderPrevNext: function () {
             // string.split('=')[1] is undefined when no = in string (this happens when it points to
             // first page) we can || 1. When we have no url at all (on first and last page), make it false
             // and remove the href.
